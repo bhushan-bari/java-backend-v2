916fd5d8127472532ff6d7433cfea57cd8beab68
Bhushan Bari <bhushanb0257@gmail.com>
2026-08-30 00:23:34 +0530

    feat: initialize Java backend project with first program

---
+++ src/com/bhushan/javabackend/Main.java
@@ -0,0 +1,10 @@
+package com.bhushan.javabackend;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java Backend!");
+        System.out.println("Module 1 - Lesson 1");
+        System.out.println("Java Version 17");
+    }
+}
