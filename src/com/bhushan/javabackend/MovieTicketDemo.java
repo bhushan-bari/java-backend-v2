47a31a2fbc50d7c89deda2c310d7ba05531e2a34
Bhushan Bari <bhushanb0257@gmail.com>
2026-09-09 23:13:43 +0530

    Add classes and objects practice programs

---
+++ src/com/bhushan/javabackend/MovieTicketDemo.java
@@ -0,0 +1,44 @@
+package com.bhushan.javabackend;
+
+class MovieTicket {
+
+    String ticketId;
+    String movieName;
+    String seatNumber;
+    double ticketPrice;
+
+    double calculateTotalPrice() {
+        return ticketPrice;
+    }
+
+    void displayTicket() {
+        System.out.println("Ticket ID    : " + ticketId);
+        System.out.println("Movie        : " + movieName);
+        System.out.println("Seat Number  : " + seatNumber);
+        System.out.println("Ticket Price : ₹" + calculateTotalPrice());
+        System.out.println();
+    }
+}
+
+public class MovieTicketDemo {
+
+    public static void main(String[] args) {
+
+        MovieTicket ticket1 = new MovieTicket();
+
+        ticket1.ticketId = "TKT501";
+        ticket1.movieName = "Interstellar";
+        ticket1.seatNumber = "A12";
+        ticket1.ticketPrice = 350;
+
+        MovieTicket ticket2 = new MovieTicket();
+
+        ticket2.ticketId = "TKT502";
+        ticket2.movieName = "Inception";
+        ticket2.seatNumber = "B08";
+        ticket2.ticketPrice = 300;
+
+        ticket1.displayTicket();
+        ticket2.displayTicket();
+    }
+}
